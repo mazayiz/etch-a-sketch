e91169ccabe1b9cf2d22deb990123fa51c5bd6ce
Melvin Ramos <melvinr0114@gmail.com>
2025-02-18 17:25:26 -0800

    Create divs using JS and place in div container

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello");
+const gridContainer = document.querySelector("#container");
+
+for (let i = 0; i < 16; i++){
+    for (let i = 0; i < 16; i++){
+        const gridDiv = document.createElement("div");
+        gridContainer.appendChild(gridDiv);
+    }
+}
