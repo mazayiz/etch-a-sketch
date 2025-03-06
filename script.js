8fec2262525588136fe4ea321ca7b74f10e139c2
Melvin Ramos <melvinr0114@gmail.com>
2025-03-05 21:08:23 -0800

    Fix grid layout and add initial grid functionality

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ const gridContainer = document.querySelector("#container");
 for (let i = 0; i < 16; i++){
     for (let i = 0; i < 16; i++){
         const gridDiv = document.createElement("div");
+        gridDiv.classList.add("gridSquare");
         gridContainer.appendChild(gridDiv);
     }
+}
+
+const markGrid = document.querySelectorAll(".gridSquare");
+
+for (let i = 0; i < markGrid.length; i++) {
+    markGrid[i].addEventListener("click", () => {
+        console.log("Fucking Success");
+      });
 }
