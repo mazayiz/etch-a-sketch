e0844dadfd707f228bfb0175df945e2c292ac709
Melvin Ramos <melvinr0114@gmail.com>
2025-04-01 21:32:21 -0700

    Add new grid size button and resize properly

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
 const gridContainer = document.querySelector("#container");
+let gridSize = 16;
+let oldGridSize = gridSize;
+gridCreate();
 
-for (let i = 0; i < 16; i++){
-    for (let i = 0; i < 16; i++){
-        const gridDiv = document.createElement("div");
-        gridDiv.classList.add("gridSquare");
-        gridContainer.appendChild(gridDiv);
+function gridCreate () {
+    const boxSize = 960/gridSize + "px";
+    for (let i = 0; i < gridSize; i++){
+        for (let j = 0; j < gridSize; j++){
+            const gridDiv = document.createElement("div");
+            gridDiv.classList.add("gridSquare");
+            gridDiv.style.flex = "1 1 auto";
+            gridDiv.style.width = boxSize;
+            gridDiv.style.height = boxSize;
+            gridContainer.appendChild(gridDiv);
+        }
     }
+    oldGridSize = gridSize*gridSize;
+    markGridEventListener();
 }
 
-const markGrid = document.querySelectorAll(".gridSquare");
-
-for (let i = 0; i < markGrid.length; i++) {
-    markGrid[i].addEventListener("mouseover", () => {
-        console.log("Fucking Success");
-        markGrid[i].style.backgroundColor = "#008080";
-      });
+function markGridEventListener () {
+        const markGrid = document.querySelectorAll(".gridSquare");
+        markGrid.forEach(gridSquare => {
+            gridSquare.addEventListener("mouseover", () => {
+                gridSquare.style.backgroundColor = "#008080";
+        });
+    })
 }
+
+const newGridButton = document.querySelector("button")
+newGridButton.addEventListener("click", () => {
+    gridSize = prompt("Choose a square grid size (max 100x100)") 
+    if (isNaN(gridSize) || gridSize < 1 || gridSize > 100) {
+        alert("Please input a number from 1-100")
+    } else {
+        for (i = 0; i < oldGridSize; i++) {
+            gridContainer.removeChild(gridContainer.children[0]);
+        }
+        gridCreate();
+    }
+});
